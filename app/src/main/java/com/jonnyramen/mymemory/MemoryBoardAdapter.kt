d70f4b341e724fa9f772563d2f87fb4862918a05
Jonathan Perry <jpper@umich.edu>
2020-12-21 14:56:18 +0900

    Finished part 3 (add the boardsize enum + create icons)

--- app/src/main/java/com/jonnyramen/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/jonnyramen/mymemory/MemoryBoardAdapter.kt
@@ -8,9 +8,14 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.jonnyramen.mymemory.models.BoardSize
 import kotlin.math.min
 
-class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
+class MemoryBoardAdapter(
+    private val context: Context,
+    private val boardSize: BoardSize,
+    private val cardImages: List<Int>
+) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
     companion object {
@@ -19,8 +24,8 @@ class MemoryBoardAdapter(private val context: Context, private val numPieces: In
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val cardWidth = parent.width / 2 - (2* MARGIN_SIZE);
-        val cardHeight = parent.height / 4 - (2* MARGIN_SIZE);
+        val cardWidth = parent.width / boardSize.getWidth() - (2* MARGIN_SIZE);
+        val cardHeight = parent.height / boardSize.getHeight() - (2* MARGIN_SIZE);
         val cardSideLength = min(cardWidth, cardHeight);
 
         val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false);
@@ -37,12 +42,13 @@ class MemoryBoardAdapter(private val context: Context, private val numPieces: In
         holder.bind(position);
     }
 
-    override fun getItemCount() = numPieces;
+    override fun getItemCount() = boardSize.numCards;
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton);
 
         fun bind(position: Int) {
+            imageButton.setImageResource(cardImages[position]);
             imageButton.setOnClickListener {
                 Log.i(TAG, "Clicked on position $position");
             }
---
+++ app/src/main/java/com/jonnyramen/mymemory/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.jonnyramen.mymemory.utils
+
+import com.jonnyramen.mymemory.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_breakfast,
+    R.drawable.ic_face,
+    R.drawable.ic_flight,
+    R.drawable.ic_heart,
+    R.drawable.ic_home,
+    R.drawable.ic_lightning,
+    R.drawable.ic_moon,
+    R.drawable.ic_plane,
+    R.drawable.ic_school,
+    R.drawable.ic_send,
+    R.drawable.ic_star,
+    R.drawable.ic_work
+)
