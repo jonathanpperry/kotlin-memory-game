7c0236c83afe530fa2800c7e1b3b506ce966f8d3
Jonathan Perry <jpper@umich.edu>
2020-12-21 18:30:54 +0900

    Finished section 5 (implementing the game logic)

--- app/src/main/java/com/jonnyramen/mymemory/MainActivity.kt
+++ app/src/main/java/com/jonnyramen/mymemory/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.jonnyramen.mymemory.models.BoardSize
 import com.jonnyramen.mymemory.models.MemoryGame
 
@@ -15,30 +17,53 @@ class MainActivity : AppCompatActivity() {
         private const val TAG = "MainActivity"
     }
 
+    private lateinit var clRoot: ConstraintLayout;
     private lateinit var rvBoard: RecyclerView;
     private lateinit var tvNumMoves: TextView;
     private lateinit var tvNumPairs: TextView;
 
+    private lateinit var memoryGame: MemoryGame;
+    private lateinit var adapter: MemoryBoardAdapter;
     private var boardSize: BoardSize = BoardSize.MEDIUM;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        clRoot = findViewById(R.id.clRoot);
         rvBoard = findViewById(R.id.rvBoard);
         tvNumMoves = findViewById(R.id.tvNumMoves);
         tvNumPairs = findViewById(R.id.tvNumPairs);
 
-        val memoryGame = MemoryGame(boardSize);
+        memoryGame = MemoryGame(boardSize);
 
-        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
+        adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
             override fun onCardClicked(position: Int) {
-                Log.i(TAG, "Card click at $position");
+                updateGameWithFlip(position);
             }
         });
+        rvBoard.adapter =adapter;
         rvBoard.setHasFixedSize(true);
         rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth());
+    }
 
+    private fun updateGameWithFlip(position: Int) {
+        // Error checking
+        if (memoryGame.haveWonGame()) {
+            // Alert the user of an invalid move
+            Snackbar.make(clRoot, "You already won!", Snackbar.LENGTH_LONG).show();
+            return;
+        }
+        if (memoryGame.isCardFaceUp(position)) {
+            // Alert the user of an invalid move
+            Snackbar.make(clRoot, "Invalid move!", Snackbar.LENGTH_LONG).show();
+            return;
+        }
+        // Actually flip over the card
+        if (memoryGame.flipCard(position)) {
+            Log.i(TAG, "Found a match!")
+        }
+        adapter.notifyDataSetChanged();
     }
 }
 
--- app/src/main/java/com/jonnyramen/mymemory/models/MemoryGame.kt
+++ app/src/main/java/com/jonnyramen/mymemory/models/MemoryGame.kt
@@ -5,12 +5,57 @@ import com.jonnyramen.mymemory.utils.DEFAULT_ICONS
 class MemoryGame(private val boardSize: BoardSize) {
 
     val cards: List<MemoryCard>;
-    val numPairsFound = 0;
+    var numPairsFound = 0;
+
+    private var indexOfSingleSelectedCard: Int? = null;
 
     init {
         val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs());
         val randomizedImages = (chosenImages+chosenImages).shuffled();
         cards = randomizedImages.map { MemoryCard(it) }
+    }
 
+    fun flipCard(position: Int):Boolean {
+        val card = cards[position];
+        var foundMatch = false;
+        if (indexOfSingleSelectedCard == null) {
+            // 0-2 cards previously flipped over => restore(maybe no-op) flip over
+            restoreCards();
+            indexOfSingleSelectedCard = position;
+        } else {
+            // 1 card previously flipped over => flip over selected + check if images match
+            foundMatch = checkForMatch(indexOfSingleSelectedCard!!, position);
+            indexOfSingleSelectedCard = null;
+        }
+
+        card.isFaceUp = !card.isFaceUp;
+        return foundMatch;
+    }
+
+    private fun checkForMatch(position1: Int, position2: Int): Boolean {
+        if (cards[position1].identifier != cards[position2].identifier) {
+            return false;
+        }
+        // Else we had a match
+        cards[position1].isMatched = true;
+        cards[position2].isMatched = true;
+        numPairsFound++;
+        return true;
+    }
+
+    private fun restoreCards() {
+        for (card in cards) {
+            if (!card.isMatched) {
+                card.isFaceUp = false;
+            }
+        }
+    }
+
+    fun haveWonGame(): Boolean {
+        return numPairsFound == boardSize.getNumPairs();
+    }
+
+    fun isCardFaceUp(position: Int): Boolean {
+        return cards[position].isFaceUp;
     }
 }
