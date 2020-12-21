ae6b8f740a4304765259ac754348de5280435c0f
Jonathan Perry <jpper@umich.edu>
2020-12-21 12:49:56 +0900

    Finished section setup the recyclerview to show memory cards

--- app/src/main/java/com/jonnyramen/mymemory/MainActivity.kt
+++ app/src/main/java/com/jonnyramen/mymemory/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jonnyramen.mymemory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +13,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvNumPairs: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        rvBoard = findViewById(R.id.rvBoard)
-        tvNumMoves = findViewById(R.id.tvNumMoves)
-        tvNumPairs = findViewById(R.id.tvNumPairs)
+        rvBoard = findViewById(R.id.rvBoard);
+        tvNumMoves = findViewById(R.id.tvNumMoves);
+        tvNumPairs = findViewById(R.id.tvNumPairs);
+
+        rvBoard.adapter = MemoryBoardAdapter(this, 8);
+        rvBoard.setHasFixedSize(true);
+        rvBoard.layoutManager = GridLayoutManager(this, 2);
+
     }
 }
+
+
