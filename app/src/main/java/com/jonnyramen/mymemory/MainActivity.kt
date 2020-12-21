4d1a2a133589042488cfa335f566e3718a750b82
Jonathan Perry <jpper@umich.edu>
2020-12-21 16:24:01 +0900

    Finished section 4 (create the memory card and game classes)

--- app/src/main/java/com/jonnyramen/mymemory/MainActivity.kt
+++ app/src/main/java/com/jonnyramen/mymemory/MainActivity.kt
@@ -2,13 +2,18 @@ package com.jonnyramen.mymemory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jonnyramen.mymemory.models.BoardSize
-import com.jonnyramen.mymemory.utils.DEFAULT_ICONS
+import com.jonnyramen.mymemory.models.MemoryGame
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 
     private lateinit var rvBoard: RecyclerView;
     private lateinit var tvNumMoves: TextView;
@@ -24,10 +29,13 @@ class MainActivity : AppCompatActivity() {
         tvNumMoves = findViewById(R.id.tvNumMoves);
         tvNumPairs = findViewById(R.id.tvNumPairs);
 
-        val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs());
-        val randomizedImages = (chosenImages+chosenImages).shuffled();
+        val memoryGame = MemoryGame(boardSize);
 
-        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, randomizedImages);
+        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
+            override fun onCardClicked(position: Int) {
+                Log.i(TAG, "Card click at $position");
+            }
+        });
         rvBoard.setHasFixedSize(true);
         rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth());
 
