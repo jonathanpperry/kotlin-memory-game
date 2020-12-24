1934a93b625eb9a5a059eff4c2836429b0284a47
Jonathan Perry <jpper@umich.edu>
2020-12-24 14:18:09 +0900

    Finished part 10 (choose image intent)

--- app/src/main/java/com/jonnyramen/mymemory/ImagePickerAdapter.kt
+++ app/src/main/java/com/jonnyramen/mymemory/ImagePickerAdapter.kt
@@ -13,9 +13,15 @@ import kotlin.math.min
 class ImagePickerAdapter(
         private val context: Context,
         private val imageUris: List<Uri>,
-        private val boardSize: BoardSize)
+        private val boardSize: BoardSize,
+        private val imageClickListener: ImageClickListener)
     : RecyclerView.Adapter<ImagePickerAdapter.ViewHolder>() {
 
+    interface ImageClickListener {
+        fun onPlaceholderClicked() {
+
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view=LayoutInflater.from(context).inflate(R.layout.card_image, parent, false);
@@ -52,6 +58,7 @@ class ImagePickerAdapter(
         fun bind() {
             ivCustomImage.setOnClickListener {
                 // Launch intent for user to select photos
+                imageClickListener.onPlaceholderClicked();
             }
         }
     }
